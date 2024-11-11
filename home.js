22cdf3d49dab7efd40cfdefe7cc98c8409ff31f0
FinerGG <94752740+FinerGG@users.noreply.github.com>
2024-11-11 22:18:30 +0700

    Create home.js

---
+++ home.js
@@ -0,0 +1,7 @@
+function setActive(element) {
+        // ลบคลาส 'active' จากลิงก์ที่มีอยู่เดิม
+        document.querySelectorAll('.hhm').forEach(link => link.classList.remove('active'));
+        
+        // เพิ่มคลาส 'active' ให้กับลิงก์ที่ถูกคลิก
+        element.classList.add('active');
+    }
