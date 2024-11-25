49e2ad3713e7a65adaa9cff0643b63b5c484f638
FinerGG <94752740+FinerGG@users.noreply.github.com>
2024-11-25 21:25:47 +0700

    Update home.js

--- public/home.js
+++ public/home.js
@@ -11,3 +11,18 @@ function delayRedirect(url, delay) {
             window.location.href = filePath;
         }, delay);
     }
+
+let previousScrollPosition = window.pageYOffset;
+const navbar = document.getElementById("navbar");
+
+window.onscroll = function() {
+  let currentScrollPosition = window.pageYOffset;
+  
+  // เปลี่ยนสีพื้นหลังของ navbar และสีของลิงก์ตามตำแหน่งการเลื่อน
+  if (currentScrollPosition === 0) {
+    navbar.classList.remove("scrolled"); // โปร่งใสเมื่ออยู่บนสุด
+  } else {
+    navbar.classList.add("scrolled"); // สีขาวเมื่อเลื่อนลง
+  }
+}
+
