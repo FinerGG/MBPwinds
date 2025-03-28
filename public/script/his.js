6a40fff109a1dfee06c8f34800e297b9a15e1988
FinerGG <94752740+FinerGG@users.noreply.github.com>
2025-03-29 02:49:23 +0700

    Update his.js

--- public/script/his.js
+++ public/script/his.js
@@ -53,17 +53,3 @@ function slideImages() {
 }
 
 setInterval(slideImages, 4000);
-
-
-document.querySelector(".menu-toggle").addEventListener("click", function() {
-  document.querySelector(".menu").classList.toggle("show");
-});
-
-function fadeToBlack(event) {
-  event.preventDefault();
-  document.body.style.transition = "opacity 0.5s";
-  document.body.style.opacity = "0";
-  setTimeout(() => {
-      window.location.href = event.target.href;
-  }, 500);
-}
