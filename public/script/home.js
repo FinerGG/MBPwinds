1933d479175ac3ba2468e01f06eb0b4d0ada5c30
FinerGG <94752740+FinerGG@users.noreply.github.com>
2025-03-30 19:20:55 +0700

    Update home.js

--- public/script/home.js
+++ public/script/home.js
@@ -61,3 +61,12 @@ function fadeToBlack(event) {
       window.location.href = event.target.href; // นำทางไปยัง href
   }, 1000); // เวลาเดียวกับ transition (1 วินาที)
 }
+
+function toggleMenu() {
+  const menu = document.querySelector('.mobile-menu');
+  const content = document.querySelector('.content');
+  const body = document.querySelector('body');
+  menu.classList.toggle('active');
+  content.classList.toggle('blur');
+  body.classList.toggle('no-scroll');
+}
